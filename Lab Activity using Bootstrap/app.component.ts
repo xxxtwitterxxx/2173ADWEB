5b592df1d7430696b5151d01124c5aef8733b1bb
xxxtwitterxxx <156291872+xxxtwitterxxx@users.noreply.github.com>
2024-01-14 15:30:06 +0800

    Add files via upload

---
+++ Lab Activity using Bootstrap/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular_data_binding';
+  clientName = "Lee Miguel"; //property
+  appliedCSS = "red"; //style
+  notAppliedCSS = false;
+  myColor = "blue";
+
+  clickCount =0
+  clickMe(){
+    this.clickCount++;
+  }
+
+  resetClickCount(){
+    this.clickCount=0;
+  }
+
+  reduceClickCount(){
+    this.clickCount--;
+  
+}
+}
